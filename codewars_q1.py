7d90f5b8a74cf130785a7195dad72c0b1f8a5e32
berkantan <a.b.gs.tan@gmail.com>
2025-05-09 16:19:03 +0300

    1'st solution

--- codewars_q1.py
+++ codewars_q1.py
@@ -1,13 +1,8 @@
 def open_or_senior(data):
-    output = []
+    output=[]
     for i,j in data:
-        if i >= 55 and j >= 7:
-            output.append('Senior')
-        else:
-            output.append('Open')
-    print(output)
-    
-
-data = [(45, 12),(55,21),(19, -2),(104, 20)]
-result = open_or_senior(data)
-print(result)
+            if i >= 55 and j >= 7:
+                output.append('Senior')
+            else:
+                output.append('Open')
+    return output
